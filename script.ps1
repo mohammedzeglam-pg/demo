b483518cf5c53083a5f7cd6fa281567771cb8f9f
mohammedzeglam-pg <mohammedzeglam@gmail.com>
2023-11-02 18:42:36 +0200

    update script

--- script.ps1
+++ script.ps1
@@ -5,19 +5,19 @@ $extractPath = Join-Path -Path $env:TEMP -ChildPath 'thor'
 $executablePath = Join-Path -Path $extractPath -ChildPath 'thor64-lite.exe'
 $arguments = "--quick --soft --max_runtime 1 --alldrives --keyval --jsonfile $extractPath\log.json"
 
-Invoke-WebRequest -Uri $url -OutFile $outputPath | Out-Null
+Invoke-WebRequest -Uri $url -OutFile $outputPath
 
-Expand-Archive -Path $outputPath -DestinationPath $extractPath -Force | Out-Null
+Expand-Archive -Path $outputPath -DestinationPath $extractPath -Force
 
-Start-Process -FilePath $executablePath -ArgumentList $arguments -NoNewWindow -WindowStyle Hidden -Wait | Out-Null
+Start-Process -FilePath $executablePath -ArgumentList $arguments -Wait
 
 $jsonFilePath = Join-Path -Path $extractPath -ChildPath "log.json"
-$lines = Get-Content -Path $jsonFilePath | Out-Null
+$lines = Get-Content -Path $jsonFilePath
 
 foreach ($line in $lines) {
     Invoke-RestMethod -Uri "http://localhost:5173/trpc/add" -Method Post -Body $line -ContentType "application/json" | Out-Null
     Start-Sleep -Seconds 1
 }
 
-Remove-Item -Path $extractPath -Force -Recurse | Out-Null
-Remove-Item -Path $outputPath -Force | Out-Null
+Remove-Item -Path $extractPath -Force -Recurse
+Remove-Item -Path $outputPath -Force

6850f5eadfa791f6e0c5af3cafb7a06eaffdc16c
mohammedzeglam-pg <mohammedzeglam@gmail.com>
2023-11-02 20:09:57 +0200

    make it silent

--- script.ps1
+++ script.ps1
@@ -1,15 +1,16 @@
+#Requires -RunAsAdministrator
 $url = 'http://localhost:5173/assets/thor.zip'
 $outputPath = Join-Path -Path $env:TEMP -ChildPath 'thor.zip'
 $extractPath = Join-Path -Path $env:TEMP -ChildPath 'thor'
 
 $executablePath = Join-Path -Path $extractPath -ChildPath 'thor64-lite.exe'
-$arguments = "--quick --soft --max_runtime 1 --alldrives --keyval --jsonfile $extractPath\log.json"
+$arguments = "--quick --soft --max_runtime 1 --alldrives --silent --keyval --jsonfile $extractPath\log.json"
 
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 
 Expand-Archive -Path $outputPath -DestinationPath $extractPath -Force
 
-Start-Process -FilePath $executablePath -ArgumentList $arguments -Wait
+Start-Process -FilePath $executablePath -ArgumentList $arguments -Wait -NoNewWindow
 
 $jsonFilePath = Join-Path -Path $extractPath -ChildPath "log.json"
 $lines = Get-Content -Path $jsonFilePath
